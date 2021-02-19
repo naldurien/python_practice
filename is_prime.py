880af23e7b6ab587db6f00f960b64098f1c3f5fc
nicole <nicole@darkstar6>
2021-02-18 22:06:08 -0500

    refactored is_prime.py

---
+++ is_prime.py
@@ -0,0 +1,13 @@
+import math
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num > 1:
+       for i in range(2, int(math.sqrt(num))+1):
+            if (num % i) == 0:
+                return False
+    return True
+
+num = int(input("Enter a number to check: "))
+print(is_prime(num))
